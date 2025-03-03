554768a394c5f09b62755bd7becad5755a57a3a5
Sahil Dari <sahil9dari@gmail.com>
2025-03-04 00:00:28 +0530

    Updated pathmanipulation.py

--- Path Manipulation/Path Manipulation while File Upload/python/pythonapp/pathmanipulation/src/pathmanipulation.py
+++ Path Manipulation/Path Manipulation while File Upload/python/pythonapp/pathmanipulation/src/pathmanipulation.py
@@ -8,17 +8,17 @@ ALLOWED_EXTENSIONS = set(["txt", "pdf"]) # this example allows the text and pdf 
 
 
 """This function checks if the filename is valid and doesn't contain any dot character in the name."""
-def is_valid_name(filename):
+def is_valid_name(filename: str) -> bool:
     name = filename.rsplit('.', 1)[0]
     regex_match = re.search(FILENAME_REGEX_PATTERN, name)
     return True if(regex_match != None) else False
 
 """This function checks for the valid file extensions."""
-def is_valid_extension(filename):
+def is_valid_extension(filename: str) -> bool:
     ext = filename.rsplit(".", 1)[1]
     return True if (ext in ALLOWED_EXTENSIONS) else False
 
-def valid_filename(filename):
+def valid_filename(filename: str) -> str:
     name, ext = filename.rsplit('.', 1)
     name = re.sub(r"\.", "", name)
     name = re.sub(r"\%[A-Za-z0-9]+", "", name)
@@ -26,10 +26,8 @@ def valid_filename(filename):
     regex_match = re.search(FILENAME_REGEX_PATTERN, name)
     return f"{regex_match.group(0)}.{ext}" # returns the sanitized filename with the extension for upload 
 
-def get_unique_filename(directory, filename):
+def get_unique_filename(directory: str, filename:str) -> str:
     name, ext = filename.rsplit(".", 1)
-    # name = valid_filename(name) 
-
     unique_filename = f"{name}.{ext}"
     count = 1
 
